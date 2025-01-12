4cc121f53df1379662b4e6e947308ef1e6ba9a60
Meir-Elbaz <meirelbaz32@gmail.com>
2025-01-12 23:39:26 +0200

    I removed firebase API details

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBrNhTH2SwadX_YNBYrGqgkmNZlQI6qIaw",
-  authDomain: "iskosher-f8bd7.firebaseapp.com",
-  projectId: "iskosher-f8bd7",
-  storageBucket: "iskosher-f8bd7.firebasestorage.app",
-  messagingSenderId: "326718613267",
-  appId: "1:326718613267:web:6074b9bebbeec0c0c59ce6",
-  measurementId: "G-PM3Z24E72J"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // אתחול Firebase
